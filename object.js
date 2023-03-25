ad6f6da82f1c0ea27dc8501b1269c32e2772b5bf
dwirustanto10 <keizaq100282@gmail.com>
2023-03-25 12:17:19 +0700

    ok

--- object.js
+++ object.js
@@ -67,32 +67,32 @@ for (var key in student) {
 // };
 // vokal.a();
 
-function countVomels(str) {
-  var result = {
-    a: 0,
-    i: 0,
-    u: 0,
-    e: 0,
-    o: 0,
-  };
+// function countVomels(str) {
+//   var result = {
+//     a: 0,
+//     i: 0,
+//     u: 0,
+//     e: 0,
+//     o: 0,
+//   };
 
-  for (var i = 0; i < str.length; i++) {
-    if (str[i] === 'a') {
-      result.a++;
-    } else if (str[i] === 'i') {
-      result.i++;
-    } else if (str[i] === 'u') {
-      result.u++;
-    } else if (str[i] === 'e') {
-      result.e++;
-    } else if (str[i] === 'o') {
-      result.o++;
-    }
-  }
-  return result;
-}
+//   for (var i = 0; i < str.length; i++) {
+//     if (str[i] === 'a') {
+//       result.a++;
+//     } else if (str[i] === 'i') {
+//       result.i++;
+//     } else if (str[i] === 'u') {
+//       result.u++;
+//     } else if (str[i] === 'e') {
+//       result.e++;
+//     } else if (str[i] === 'o') {
+//       result.o++;
+//     }
+//   }
+//   return result;
+// }
 
-console.log(countVomels('Buka puasa bersama teman kuliah'));
+// console.log(countVomels('Buka puasa bersama teman kuliah'));
 
 // function countVotes(votes) {
 //   var result = {
